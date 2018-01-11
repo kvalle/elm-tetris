96dd36e0a80d3bdf9d2951b92b7a0b3ea0b15c28
Kjetil Valle <kjetil.valle@gmail.com>
2018-01-11 21:59:52 +0100

    Refactor View module

--- src/View.elm
+++ src/View.elm
@@ -8,58 +8,15 @@ import Color
 import Model exposing (Model)
 import Config
 import Types.Common exposing (Color(..), GameState(..))
-import Types.Board as Board exposing (Cell(..))
+import Types.Board as Board exposing (Board, Cell(..))
+import Types.Pos as Pos exposing (Pos)
 
 
 view : Model -> Html msg
 view model =
     div [ class "game" ]
         [ div [ class "board" ]
-            [ let
-                cellColor cell =
-                    case cell of
-                        Empty ->
-                            Color.white
-
-                        Filled Blue ->
-                            Color.blue
-
-                        Filled Red ->
-                            Color.red
-
-                        Filled Green ->
-                            Color.green
-
-                        Filled Yellow ->
-                            Color.yellow
-
-                        Filled Purple ->
-                            Color.purple
-
-                toCanvasCoord : Int -> Int -> ( Float, Float )
-                toCanvasCoord row col =
-                    ( toFloat <| (col - (Config.width // 2)) * Config.pixelSize
-                    , toFloat <| (Config.height - 1 - (row + (Config.height // 2))) * Config.pixelSize
-                    )
-
-                board : List Collage.Form
-                board =
-                    model.board
-                        |> Board.addPiece model.piece
-                        |> Board.toPosList
-                        |> List.map
-                            (\( pos, cell ) ->
-                                Collage.square Config.pixelSize
-                                    |> Collage.filled (cellColor cell)
-                                    |> Collage.move (toCanvasCoord pos.row pos.col)
-                            )
-              in
-                board
-                    |> Collage.collage
-                        (Config.width * Config.pixelSize - Config.pixelSize // 2)
-                        (Config.height * Config.pixelSize - Config.pixelSize // 2)
-                    |> Element.toHtml
-            ]
+            [ tetrisCanvas model ]
         , div [ class "info" ]
             [ div [ class "title" ] [ text "Tetris" ]
             , div [ class "status" ]
@@ -76,3 +33,57 @@ view model =
                 ]
             ]
         ]
+
+
+tetrisCanvas : Model -> Html msg
+tetrisCanvas model =
+    model
+        |> renderBoard
+        |> Collage.collage
+            (Config.width * Config.pixelSize - Config.pixelSize // 2)
+            (Config.height * Config.pixelSize - Config.pixelSize // 2)
+        |> Element.toHtml
+
+
+renderBoard : Model -> List Collage.Form
+renderBoard model =
+    model.board
+        |> Board.addPiece model.piece
+        |> Board.toPosList
+        |> List.map (uncurry renderCell)
+
+
+renderCell : Pos -> Cell -> Collage.Form
+renderCell pos cell =
+    Collage.square Config.pixelSize
+        |> Collage.filled (cellColor cell)
+        |> Collage.move (toCanvasCoord pos)
+
+
+cellColor : Cell -> Color.Color
+cellColor cell =
+    case cell of
+        Empty ->
+            Color.white
+
+        Filled Blue ->
+            Color.blue
+
+        Filled Red ->
+            Color.red
+
+        Filled Green ->
+            Color.green
+
+        Filled Yellow ->
+            Color.yellow
+
+        Filled Purple ->
+            Color.purple
+
+
+toCanvasCoord : Pos -> ( Float, Float )
+toCanvasCoord pos =
+    ( toFloat <| (pos.col - (Config.width // 2)) * Config.pixelSize
+    , toFloat <| (Config.height - 1 - (pos.row + (Config.height // 2))) * Config.pixelSize
+    )
