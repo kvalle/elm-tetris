5f4d5ad5ac30ae5ed9bd5ec7ebc309039278dd23
Kjetil Valle <kjetil.valle@gmail.com>
2018-01-12 16:28:16 +0100

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+npm run build
+
+git checkout gh-pages
+for file in $(ls -1 build/)
+do
+  cp "build/$file" .
+  git add $file
+  echo "updated $file"
+done
+
+git commit -m "Deploy updated version to GitHub Pages"
+git push origin gh-pages
+git checkout master
